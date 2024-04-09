4b62825a39fd49808586b3e1348598b741ee953a
gordonmenzies <heymish96@gmail.com>
2024-04-09 12:44:39 +0100

    icons imported, 4th navigation button added

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,15 +1,22 @@
 import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LeaderboardIcon from "@mui/icons-material/Leaderboard";
+import HomeIcon from "@mui/icons-material/Home";
+import MapIcon from "@mui/icons-material/Map";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import "./Navigation.scss";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 const Navigation = () => {
   const [value, setValue] = useState(0);
+
+  // allow the buttons to navigate on click
+  // change colour when appropriate menu item is selected
+
   return (
-    <Box sx={{ width: 500 }}>
+    <Box sx={{ width: "100%" }}>
       <BottomNavigation
         showLabels
         value={value}
@@ -18,12 +25,25 @@ const Navigation = () => {
         }}
       >
         <BottomNavigationAction
-          label="Recents"
-          icon={<RestoreIcon />}
-          sx={{ color: "black" }}
+          label="Home"
+          icon={<HomeIcon />}
+          sx={{ "& .MuiBottomNavigationAction-label": { color: "black" } }}
         />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction
+          label="Tasks"
+          icon={<MapIcon />}
+          sx={{ "& .MuiBottomNavigationAction-label": { color: "black" } }}
+        />
+        <BottomNavigationAction
+          label="Calendar"
+          icon={<CalendarMonthIcon />}
+          sx={{ "& .MuiBottomNavigationAction-label": { color: "black" } }}
+        />
+        <BottomNavigationAction
+          label="Leaderboard"
+          icon={<LeaderboardIcon />}
+          sx={{ "& .MuiBottomNavigationAction-label": { color: "black" } }}
+        />
       </BottomNavigation>
     </Box>
   );
