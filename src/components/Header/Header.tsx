e36f9d71e2e28d810b485f66e74f9f14528444bf
Michaela <michaelachan45@gmail.com>
2024-04-09 16:52:57 +0100

    added link to profile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,6 @@
-import "./Header.scss"
+import "./Header.scss";
+import { Link } from "react-router-dom";
+
 
 type HeaderProps = {
     title: string;
@@ -12,9 +14,13 @@ const Header = ({title, subtitle, image}: HeaderProps) => {
 
     return (
         <header className="header">
+    
             <div className="header__image-div">
+                <Link to="/profile">
                 <img className="header__image-div header__image-div--image" src={image} />
+                </Link>
             </div>
+        
             <div className="header__title-div">
                 <h4 className="header__title-div header__title-div--title">{title}</h4>
                 <h1 className="header__title-div header__title-div--subtitle">{subtitle}</h1>
