2590c260ad3f70ed67ed2190465d58875eaa5269
Athish Thayalan <tathish@hotmail.co.uk>
2024-04-09 15:40:45 +0100

    styling

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,7 +4,9 @@ import LeaderboardIcon from "@mui/icons-material/Leaderboard";
 import HomeIcon from "@mui/icons-material/Home";
 import MapIcon from "@mui/icons-material/Map";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import { Link } from "react-router-dom";
 import { useState } from "react";
+import "./Navigation.scss";
 
 const Navigation = () => {
   const [value, setValue] = useState(0);
@@ -12,19 +14,35 @@ const Navigation = () => {
   return (
     <Box sx={{ width: "100%", position: "absolute", bottom: 0 }}>
       <BottomNavigation
+        className="bottom-navigation"
         showLabels
         value={value}
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-        <BottomNavigationAction label="Tasks" icon={<MapIcon />} />
-        <BottomNavigationAction label="Calendar" icon={<CalendarMonthIcon />} />
         <BottomNavigationAction
-          label="Leaderboard"
-          icon={<LeaderboardIcon />}
+          className="bottom-navigation__item"
+          label="Home"
+          icon={<HomeIcon />}
         />
+        <BottomNavigationAction
+          className="bottom-navigation__item"
+          label="Tasks"
+          icon={<MapIcon />}
+        />
+        <BottomNavigationAction
+          className="bottom-navigation__item"
+          label="Calendar"
+          icon={<CalendarMonthIcon />}
+        />
+        <Link to={"/"}>
+          <BottomNavigationAction
+            className="bottom-navigation__item"
+            label="Leaderboard"
+            icon={<LeaderboardIcon />}
+          />
+        </Link>
       </BottomNavigation>
     </Box>
   );
