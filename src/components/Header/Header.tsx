fc67fbc28a18a660379ee842fe651c4126b04d39
Michaela <michaelachan45@gmail.com>
2024-04-09 14:19:12 +0100

    some styling added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,11 @@ type HeaderProps = {
     title: string;
     subtitle ?: string
     image ?: string;
+    
 }
 
 const Header = ({title, subtitle, image}: HeaderProps) => {
+    // "today" should only be used in the Home page header.
     const today = new Date(),
         date = today.getDate() + " " +  today.toLocaleString('default', { month: 'long' }) + " " +  today.getFullYear();
     
@@ -14,12 +16,14 @@ const Header = ({title, subtitle, image}: HeaderProps) => {
 
     return (
         <header className="header">
-            <div className="header__image">
-                <img src={image} />
+            <div className="header__image-div">
+                <img className="header__image" src={image} />
             </div>
-            <h4 className="header__title">{title}</h4>
-            <h1 className="header__subtitle">{subtitle}</h1>
+            <div className="header__title-div">
+                <h4 className="header__title">{title}</h4>
+                <h1 className="header__subtitle">{subtitle}</h1>
             <p>{date}</p>
+            </div>
         </header>
     )
 }
