ae1916713f7d2f0acafaaf9cba5da951d39ce8e2
Athish Thayalan <tathish@hotmail.co.uk>
2024-04-09 16:17:51 +0100

    Changed color for onclick property of nav items

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,12 +7,7 @@ const Home = () => {
       <h1>Page Heading</h1>
       <h2>Section Heading</h2>
       <h3>Panel Heading</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-        ducimus maiores vitae enim sequi dignissimos, suscipit temporibus
-        voluptatum eius recusandae molestias cum quod quam necessitatibus quidem
-        quibusdam dolorem? Porro, dolore!{" "}
-      </p>
+      <p>hi</p>
       <Navigation />
     </Layout>
   );
