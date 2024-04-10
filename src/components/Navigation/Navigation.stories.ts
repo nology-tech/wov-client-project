0a20c1cc7f0fc73b17ba5ddb15d37425b8cbffc0
Athish Thayalan <120419238+AthishThayalan@users.noreply.github.com>
2024-04-10 16:37:53 +0100

    Component navigation (#8)
    * Added branches conventions
    
    * styling conventions added
    
    * created necessary files for nav component using MUI
    
    * icons imported, 4th navigation button added
    
    * nav fixed to bottom.
    
    * mui styling problems identified
    
    * styling
    
    * Routing links made.
    
    * Changed color for onclick property of nav items
    
    * updates to the readme added
    
    * added home svg
    
    * correct svg's added
    
    * reverted to original icons
    
    * Commented code . You can highlight your on custom SVG when its active
    
    * Tests for nav complete
    
    * storybook linked sucessfully no meaningful stories
    
    * (feat): Added nav index prop for more control in story book
    
    * Adjusted tests for component changes
    
    * updates reactRouterParameters within stories
    
    * removed comments
    
    * Removed comments and adjusted on click colouring
    
    * leaderboard story added
    
    * changed color for active nav items to use a variable.
    
    * removed styled components dependency
    
    * passed prop into Navigation for home page
    
    * DELETED unused icons
    
    * fixed navigation changes suggested on pull request
    
    * updated based on github pull request checks, memory router removed and render removed from imports on navigation.test.tx
    
    * removed extra css files that were unnecessary
    
    * app home imports adressed
    
    * Added top shadow, removed p tag , and changed description of component.
    
    ---------
    
    Co-authored-by: charlie-robin <charlie.robin.richardson@gmail.com>
    Co-authored-by: gordonmenzies <heymish96@gmail.com>

---
+++ src/components/Navigation/Navigation.stories.ts
@@ -0,0 +1,110 @@
+// Button.stories.ts
+
+import type { Meta, StoryObj } from "@storybook/react";
+import {
+  reactRouterParameters,
+  withRouter,
+} from "storybook-addon-remix-react-router";
+
+import Navigation from "./Navigation.tsx";
+
+const meta: Meta<typeof Navigation> = {
+  title: "Components/Navigation",
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component: "A Material UI Navigation Component",
+      },
+    },
+  },
+  component: Navigation,
+  decorators: [withRouter],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Navigation>;
+
+/**
+ * Stories
+ * @returns button with label and variant
+ */
+
+export const Home: Story = {
+  parameters: {
+    reactRouter: reactRouterParameters({
+      routing: { path: "/" },
+    }),
+  },
+};
+
+export const Tasks: Story = {
+  parameters: {
+    reactRouter: reactRouterParameters({
+      routing: { path: "/tasks" },
+    }),
+  },
+  args: {
+    navActionIndex: 1,
+  },
+};
+
+export const Calendar: Story = {
+  parameters: {
+    reactRouter: reactRouterParameters({
+      routing: { path: "/calendar" },
+    }),
+  },
+  args: {
+    navActionIndex: 2,
+  },
+};
+
+export const Leaderboard: Story = {
+  parameters: {
+    reactRouter: reactRouterParameters({
+      routing: { path: "/leaderboard" },
+    }),
+  },
+  args: {
+    navActionIndex: 3,
+  },
+};
+
+/**
+ * Parameters
+ * @returns parameters with the description of the story
+ */
+
+Home.parameters = {
+  docs: {
+    description: {
+      story: "The Navigation with Home highlighted",
+    },
+  },
+};
+
+Tasks.parameters = {
+  docs: {
+    description: {
+      story: "The Navigation with Tasks is highlighted",
+    },
+  },
+};
+
+Calendar.parameters = {
+  docs: {
+    description: {
+      story: "The Navigation with Calendar is highlighted",
+    },
+  },
+};
+
+Leaderboard.parameters = {
+  docs: {
+    description: {
+      story: "The Navigation with Leaderboard is highlighted",
+    },
+  },
+};
