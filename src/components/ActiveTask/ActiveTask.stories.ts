f8fd1c91e29d71cd728e665c4fd4bec55b26f760
Emma Oyetey <emmaflookis@hotmail.com>
2024-04-10 13:04:11 +0100

    removed comments from ActiveTask files

--- src/components/ActiveTask/ActiveTask.stories.ts
+++ src/components/ActiveTask/ActiveTask.stories.ts
@@ -1,15 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
-//import {fn} from '@storybook/test';
 import ActiveTask from "./ActiveTask";
 
 const meta: Meta = {
 
     title: 'Example/ActiveTask',
     component: ActiveTask,
-    //parameters: {layout: 'centered', }, //centre searchbox in the storybook
-    //argTypes: {handleTaskCompletion: {action: 'input'},}, //log input event using addon-actions
-    //args: {handleTaskCompletion: fn() }, //default handleinput action
-    tags: ['autodocs'], //add tags for documentation
+    tags: ['autodocs'], 
 
 } satisfies Meta<typeof ActiveTask>; 
 
