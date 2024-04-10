139db0924aa51414b1fbded154905b925b2fb12a
charlie-robin <charlie.robin.richardson@gmail.com>
2024-04-10 11:21:40 +0100

    Added simple file upload to storage and retrieval

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,42 @@
 import Layout from "../../components/Layout/Layout";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../../firebase";
+import { FormEvent, useState } from "react";
+
+const storage = getStorage(app);
 
 const Home = () => {
+  // Create a root reference
+  const [imgPath, setImg] = useState("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const input = form[0] as HTMLInputElement;
+    const fileRef = ref(storage, "Test");
+
+    if (input.files == null) {
+      return;
+    }
+    uploadBytes(fileRef, input.files[0]).then((upload) => {
+      getDownloadURL(upload.ref).then((url) => {
+        setImg(url);
+      });
+    });
+  };
+
   return (
     <Layout>
-      <h1>Page Heading</h1>
-      <h2>Section Heading</h2>
-      <h3>Panel Heading</h3>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
-        dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
-        maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
-        necessitatibus.
-      </p>
+      {imgPath && <img src={imgPath} />}
+      <form action="" onSubmit={handleSubmit}>
+        <input type="file" name="" id="" />
+        <button type="submit">submit</button>
+      </form>
     </Layout>
   );
 };
