cb03ebd0eed7677a6b0fef8c6082c880b42a6802
Athish Thayalan <tathish@hotmail.co.uk>
2024-04-10 17:02:39 +0100

    removed an import

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,10 @@
 import Layout from "../../components/Layout/Layout";
-import Navigation from "../../components/Navigation/Navigation";
 import Profile from "../Profile/Profile";
 
 const Home = () => {
   return (
     <Layout>
-      <h1>Page Heading</h1>
-      <h2>Section Heading</h2>
-      <h3>Panel Heading</h3>
+      <Profile />
     </Layout>
   );
 };
