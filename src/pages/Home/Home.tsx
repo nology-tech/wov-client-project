1db9383013de9e6f29c1d216c8b236b65b069b04
gordonmenzies <heymish96@gmail.com>
2024-04-11 12:51:02 +0100

    further changes to home page styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -24,9 +24,11 @@ const Home = () => {
         image="https://s3-alpha-sig.figma.com/img/9003/31aa/6dd32f49cc5bf6f29932b0481a2af856?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FrjVJj2XFAnoifxYaGoHI0CfbBKprDFMJqDeV1ur21HRwi~j1ZYNRCW-3LPZchYU-brnUydqEOyJ8eUfwA9nB6ATjPomas0~pIPizR9gu2Je79kN9EP1pmgZjI3K6aKUXz7IoMgEErOcHxjXyt42pBlu1Ym8~HfjFPcrLW1igtrvA6JO9y997SJDeldC1ENK0Y8myy9wViW54XoUX1ripSdMlgX9NG2pa--C1P1eafFfIzyQyWrB-Uz8e9eWq0va0JvsDInxsaRT1wOvxHVAxGc~nyK5VJwcSVlplUeTEEL~fMMPrn31K8dbJHlxd0bGEGobas-1vWFzvzv~01QwUw__"
       />
       <div className="daily-tasks">
-        <Link to="/daily-tasks">
-          <Button label="VIEW TODAY's TASKS" variant="primary"></Button>
-        </Link>
+        <div className="daily-tasks__button">
+          <Link to="/daily-tasks">
+            <Button label="VIEW TODAY's TASKS" variant="primary"></Button>
+          </Link>
+        </div>
         <h2 className="daily-tasks__header">
           Welcome to the home page for your 10 week Way of the Viking adventure
         </h2>
