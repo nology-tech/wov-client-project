3300d8778f7d3dfb0459349529d186d0c64c39e5
Michaela <michaelachan45@gmail.com>
2024-04-11 12:44:33 +0100

    storybook added

--- src/components/LeaderboardCard/LeaderboardCard.stories.tsx
+++ src/components/LeaderboardCard/LeaderboardCard.stories.tsx
@@ -15,7 +15,7 @@ type Story = StoryObj<typeof LeaderboardCard>;
 export const WithAllComponents: Story = {
   args: {
     name: "Sam Joyce",
-    profileImage: "",
+    profileImage: "https://www.path.cam.ac.uk/sites/www.path.cam.ac.uk/files/media/profile_image_placeholder_41.png" ,
     totalScore: 200
   },
 };
