d3718f4c416395b953ef2b3a047e3567e03a44b3
Michaela <michaelachan45@gmail.com>
2024-04-11 10:19:21 +0100

    test passes

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -3,9 +3,9 @@ import Header from "./Header";
 import { customRender } from "../../utils/testUtils";
 
 describe("should pass Header component tests", () => {
-  it("should render title", () => {
-    customRender(<Header title={"Test Title"} />);
-    const headerTitle = screen.getByText("Test Title");
+  it("should render subtitle", () => {
+    customRender(<Header subtitle={"Test Subtitle"} />);
+    const headerTitle = screen.getByText("Test Subtitle");
     expect(headerTitle).toBeInTheDocument();
   });
 });
