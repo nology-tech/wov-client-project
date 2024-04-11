2d917d189f1d8dd610bff5e4a32833314a7e5d59
Cheryl Lee <clee0937@gmail.com>
2024-04-11 10:49:57 +0100

    amended comments in stories

--- src/components/CompletedTask/CompletedTask.stories.tsx
+++ src/components/CompletedTask/CompletedTask.stories.tsx
@@ -1,19 +1,23 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import CompletedTask from "./CompletedTask";
 
-/** The CompletedTask component has 5 props: taskHeading(string), category(string), points(number), description(string) and image(url as a string). The image prop is optional.*/
-
 const meta = {
   title: "Components/CompletedTask",
   tags: ["autodocs"],
   component: CompletedTask,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "The CompletedTask component has 5 props: taskHeading(string), category(string), points(number), description(string) and image(url as a string). The image prop is optional.",
+      },
+    },
+  },
 } as Meta;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-
-/** This is a story where the component has an image prop i.e the user has uploaded an image. */
 
 export const CompletedTaskWithMedia: Story = {
   args: {
@@ -26,13 +30,29 @@ export const CompletedTaskWithMedia: Story = {
   },
 };
 
-/** This is a story where the component doesn't have an image prop i.e the user hasn't uploaded an image. */
-
 export const CompletedTaskWithoutMedia: Story = {
   args: {
     taskHeading: "Run 5k",
     category: "Fitness",
     points: 10,
     description: "Run as fast as you can",
+  },
+};
+
+CompletedTaskWithMedia.parameters = {
+  docs: {
+    description: {
+      story:
+        "This is a story where the component has an image prop i.e the user has uploaded an image.",
+    },
+  },
+};
+
+CompletedTaskWithoutMedia.parameters = {
+  docs: {
+    description: {
+      story:
+        "This is a story where the component doesn't have an image prop i.e the user hasn't uploaded an image.",
+    },
   },
 };
