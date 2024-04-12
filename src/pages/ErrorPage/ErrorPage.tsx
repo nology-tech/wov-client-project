f8212c89729d0a5f099cc7c51dee11f2c5e99172
Athish Thayalan <tathish@hotmail.co.uk>
2024-04-12 15:51:37 +0100

    Change imaged to SVG and got rid of bottom empty content

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -2,15 +2,19 @@ import "./ErrorPage.scss";
 import Button from "../../components/Button/Button";
 import errorMessage from "../../../public/assets/images/error-message.png";
 import { Link } from "react-router-dom";
+import ErrorSVG from "./ErrorSVG";
 
 const ErrorPage = () => {
   return (
     <div className="error-page">
-      <h1 className="error-page__heading-first">OOPS.</h1>
-      <h1 className="error-page__heading-second">
+      <h1 className="error-page__heading-first">
+        OOPS.
+        <br />
         LOOKS LIKE YOU'VE TAKEN A WRONG TURN.
       </h1>
-      <img className="error-page__image" src={errorMessage}></img>
+
+      {/* <img className="error-page__image" src={errorMessage}></img> */}
+      <ErrorSVG />
       <Link to="/home">
         <div className="error-page__button">
           <Button label={"Home"}></Button>
