baa0b24def711e174e2cbb14595ba578cd7144b5
Michaela <michaelachan45@gmail.com>
2024-04-12 12:39:30 +0100

    some styling done

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -2,6 +2,8 @@ import "./Leaderboard.scss";
 import LeaderboardCard from "../../components/LeaderboardCard/LeaderboardCard";
 import { UserProfile } from "../../utils/mockData";
 import { Link } from "react-router-dom";
+import Header from "../../components/Header/Header";
+import Navigation from "../../components/Navigation/Navigation";
 
 const Leaderboard = ({ users }: { users: UserProfile[] }) => {
     const sortUserByScore = () => {
@@ -18,14 +20,20 @@ const Leaderboard = ({ users }: { users: UserProfile[] }) => {
   
     return (
       <div className="leaderboard">
+        <Header subtitle="Leaderboard"/>
+        <div className="leaderboard__cards">
         {sortUserByScore().map((user) => (
           <LeaderboardCard
             key={user.id} 
             name={user.name}
-            profileImage={user.img}
+            profileImage={user.img ?? "./assets/images/default-profile-image.png"}
             totalScore={user.score}
           />
-        ))}
+        ))};
+        </div>
+        <div>
+        <Navigation navActionIndex={0} />
+        </div>
       </div>
     );
   };
