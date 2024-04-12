efe00ec7cc34df4e38f94d36eef07c244b8fdb26
Samuel Raducan <samuel.thembp@gmail.com>
2024-04-12 12:48:32 +0100

    Show forms dynamically

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,11 +1,11 @@
-import "./Register.scss";
-import Button from "../Button/Button";
-import arrowLeft from "../../assets/images/arrow-left.png";
-import { Link } from "react-router-dom";
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+import arrowLeft from "../../assets/images/arrow-left.png";
 import { firebaseConfig } from "../../utils/testUtils";
+import Button from "../Button/Button";
+import "./Register.scss";
 
 initializeApp(firebaseConfig);
 const emptyFormData = {
@@ -18,11 +18,19 @@ const emptyFormData = {
 
 const Register = () => {
   const [formData, setFormData] = useState(emptyFormData);
-  const [passwordMatchError, setPasswordMatchError] = useState("");
+  const [passwordMatchError, setPasswordMatchError] = useState<string>("");
+  const [showSecondForm, setShowSecondFrom] = useState<boolean>(false);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
+  };
+
+  const handleNext = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (formData.firstName && formData.lastName) {
+      setShowSecondFrom(true);
+    }
   };
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
@@ -41,7 +49,6 @@ const Register = () => {
       );
       console.log("user registered successfully", userCredential.user);
     } catch (error) {
-      console.error("Error registering user", error);
       setFormData(emptyFormData);
       setPasswordMatchError((error as Error).message);
     }
@@ -50,7 +57,7 @@ const Register = () => {
   return (
     <section className="register">
       <div className="register__icon--container">
-        <Link to={"#"}>
+        <Link to={`#`}>
           <img
             className="register__icon--arrow"
             src={arrowLeft}
@@ -60,77 +67,79 @@ const Register = () => {
       </div>
       <h2 className="register__heading">Create Account</h2>
 
-      <form onSubmit={handleSubmit} className="register__form" action="#">
-        <label className="register__form--label" htmlFor="">
-          First Name
-        </label>
-        <input
-          id="firstName"
-          name="firstName"
-          value={formData.firstName}
-          className="register__form--input"
-          type="text"
-          placeholder="John"
-          onChange={handleChange}
-        />
-        <label className="register__form--label" htmlFor="">
-          Last Name
-        </label>
-        <input
-          id="lastName"
-          name="lastName"
-          value={formData.lastName}
-          className="register__form--input with-margin-bottom"
-          type="text"
-          placeholder="Smith"
-          onChange={handleChange}
-        />
-        <Button label="Next" />
-      </form>
-
-      <form onSubmit={handleSubmit} className="register__form" action="#">
-        <label className="register__form--label" htmlFor="">
-          Email Address
-        </label>
-        <input
-          id="email"
-          name="email"
-          value={formData.email}
-          className="register__form--input"
-          type="email"
-          placeholder="you@example.com"
-          onChange={handleChange}
-        />
-        <label className="register__form--label" htmlFor="">
-          Password
-        </label>
-        <input
-          id="password"
-          name="password"
-          value={formData.password}
-          className="register__form--input"
-          type="password"
-          placeholder="Your password"
-          onChange={handleChange}
-        />
-        <label className="register__form--label" htmlFor="">
-          Confirm Password
-        </label>
-        <input
-          id="confirmPassword"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          className="register__form--input with-margin-bottom"
-          type="password"
-          onChange={handleChange}
-        />
-        {passwordMatchError && (
-          <p className="register__error-message">{passwordMatchError}</p>
-        )}
-        {/* WHEN REGISTERING IS SUCCESSFUL -> LINK TO SIGN PAGE */}
-        {/* ALSO, SHOW A MESSAGE TO THE USER WITH CONGRATULATIONS FOR CREATING AN ACCOUNT */}
-        <Button label="SIGN UP" />
-      </form>
+      {!showSecondForm ? (
+        <form onSubmit={handleSubmit} className="register__form" action="#">
+          <label className="register__form--label" htmlFor="">
+            First Name
+          </label>
+          <input
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            className="register__form--input"
+            type="text"
+            placeholder="John"
+            onChange={handleChange}
+          />
+          <label className="register__form--label" htmlFor="">
+            Last Name
+          </label>
+          <input
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            className="register__form--input with-margin-bottom"
+            type="text"
+            placeholder="Smith"
+            onChange={handleChange}
+          />
+          <Button label="Next" onClick={handleNext} />
+        </form>
+      ) : (
+        <form onSubmit={handleSubmit} className="register__form" action="#">
+          <label className="register__form--label" htmlFor="">
+            Email Address
+          </label>
+          <input
+            id="email"
+            name="email"
+            value={formData.email}
+            className="register__form--input"
+            type="email"
+            placeholder="you@example.com"
+            onChange={handleChange}
+          />
+          <label className="register__form--label" htmlFor="">
+            Password
+          </label>
+          <input
+            id="password"
+            name="password"
+            value={formData.password}
+            className="register__form--input"
+            type="password"
+            placeholder="Your password"
+            onChange={handleChange}
+          />
+          <label className="register__form--label" htmlFor="">
+            Confirm Password
+          </label>
+          <input
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            className="register__form--input with-margin-bottom"
+            type="password"
+            onChange={handleChange}
+          />
+          {passwordMatchError && (
+            <p className="register__error-message">{passwordMatchError}</p>
+          )}
+          {/* WHEN REGISTERING IS SUCCESSFUL -> LINK TO SIGN PAGE */}
+          {/* ALSO, SHOW A MESSAGE TO THE USER WITH CONGRATULATIONS FOR CREATING AN ACCOUNT */}
+          <Button label="SIGN UP" />
+        </form>
+      )}
     </section>
   );
 };
