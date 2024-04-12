7fc3f6a8c3ae79ec36301ca6416231fee5b00e66
Michaela <michaelachan45@gmail.com>
2024-04-12 10:52:04 +0100

    changed styling per PR

--- src/components/LeaderboardCard/LeaderboardCard.tsx
+++ src/components/LeaderboardCard/LeaderboardCard.tsx
@@ -1,5 +1,4 @@
 import "./LeaderboardCard.scss"
-import fallbackProfile from "../../../public/assets/images/default-profile-image.png"
 import { capitalisedFirstLetters} from "../../utils/capitalisedFirstLetters"
 
 type LeaderboardCardProps = {
@@ -15,7 +14,7 @@ const LeaderboardCard = ({name, profileImage, totalScore} : LeaderboardCardProps
     return(
         <div className="leaderboard-card">
             <div className="leaderboard-card__img-div">
-            <img src={profileImage ?? fallbackProfile }  className="leaderboard-card__img-div leaderboard-card__img-div--img"/>
+            <img src={profileImage ?? "./assets/images/default-profile-image.png" }  className="leaderboard-card__img-div leaderboard-card__img-div--img"/>
             </div>
             <p className="leaderboard-card__name">{myName}</p>
             <div>
