de486b23aa98b4b117857a57fd352f7900a8fd15
Todd <todd.griffin61@outlook.com>
2024-04-15 14:15:50 +0100

    fixed error

--- src/utils/Login.ts
+++ src/utils/Login.ts
@@ -1,13 +1,16 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, signInWithCustomToken } from "firebase/auth";
+import { app } from "../firebase";
 
-const auth = getAuth();
-signInWithEmailAndPassword(auth, "test@example.com" , "123456").then((userCredential) => {
+const auth = getAuth(app);
+signInWithEmailAndPassword(auth, "test@example.com" , "123456")
+// signInWithCustomToken(auth, "qDjHyzko7ehZKSOSHe0uHJ0KEjR2")
+    .then((userCredential) => {
     const user = userCredential.user;
     console.log(user);
 })
 .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    console.log(errorCode)
-    console.log(errorMessage)
+    console.log(errorCode);
+    console.log(errorMessage);
 })
