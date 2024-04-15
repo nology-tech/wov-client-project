047c627645e29679c2fa792277f6987f30b30f19
Todd <todd.griffin61@outlook.com>
2024-04-15 15:12:28 +0100

    added routing to sign-in page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,63 +2,71 @@ import "./Login.scss";
 import Button from "../../components/Button/Button";
 import arrowLeft from "../../assets/images/arrow-left.png";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const emptyFormData = {
-    email: "",
-    password: "",
+  email: "",
+  password: "",
 }
 
 export const Login = () => {
-    const [showSecondForm, setShowSecondFrom] = useState<boolean>(false);
+  const [showSecondForm, setShowSecondFrom] = useState<boolean>(false);
 
-    // const handleNext = (event: React.MouseEvent<HTMLButtonElement>) => {
-    //     event.preventDefault();
-    //     if (formData.firstName && formData.lastName) {
-    //       setShowSecondFrom(true);
-    //     }
-    //   };
+  // const handleNext = (event: React.MouseEvent<HTMLButtonElement>) => {
+  //     event.preventDefault();
+  //     if (formData.firstName && formData.lastName) {
+  //       setShowSecondFrom(true);
+  //     }
+  //   };
 
-    // const handlePrevious = () => {
-    //     showSecondForm ? setShowSecondFrom(false) : navigate(-1);
-    //   };
-    return(
-        <section className="register">
+  // const handlePrevious = () => {
+  //     showSecondForm ? setShowSecondFrom(false) : navigate(-1);
+  //   };
+
+  const handlePrevious = () => {
+    navigate(-1);
+  };
+
+  const navigate = useNavigate();
+
+  return (
+    <section className="register">
       <div className="register__icon--container">
         <img
-          // onClick={handlePrevious}
+          onClick={handlePrevious}
           className="register__icon--arrow"
           src={arrowLeft}
-          alt="&lt;"
+          alt="Arrow Left Icon"
         />
       </div>
       <h2 className="register__heading">Sign In</h2>
       <form className="register__form" action="#">
-          <label className="register__label" htmlFor="email">
-            Email Address
-          </label>
-          <input
-            id="email"
-            name="email"
-            // value={formData.email}
-            className="register__input"
-            type="email"
-            placeholder="you@example.com"
-            // onChange={handleChange}
-          />
-          </form>
-          <input
-            id="password"
-            name="password"
-            // value={formData.password}
-            className="register__input"
-            type="password"
-            placeholder="Your password"
-            // onChange={handleChange}
-          />
-          <Button label="Sign In" />
-      </section>
-    )
+        <label className="register__label" htmlFor="email">
+          Email Address
+        </label>
+        <input
+          id="email"
+          name="email"
+          // value={formData.email}
+          className="register__input"
+          type="email"
+          placeholder="you@example.com"
+        // onChange={handleChange}
+        />
+      </form>
+      <input
+        id="password"
+        name="password"
+        // value={formData.password}
+        className="register__input"
+        type="password"
+        placeholder="Your password"
+      // onChange={handleChange}
+      />
+      <Button label="Sign In" />
+    </section>
+  )
 }
 
 export default Login;
