815468ecb85a604a1a0d84ff651c8e0f826a07bd
Hamish Lawson <77970775+gordonmenzies@users.noreply.github.com>
2024-04-15 12:45:48 +0100

    Feature error page (#23)
    * basic content and styling for home page
    
    * further changes to home page styling
    
    * content added to mimic display on figma file
    
    * styling adjustments to mimic figma file
    
    * tests written for home, to show button is displaying correclty
    
    * navigation now sticks to bottom of screen when scrolling
    
    * Adjusted line height according to our conventions
    
    * commit before submitting home component for pull request
    
    * added test to check button route's to new page
    
    * npm install, npm run list, npm run test
    
    * 100%width for content
    
    * removeed border
    
    * updated home.ts
    
    * updated home test file
    
    * Added new text for home page
    
    * Added quote.
    
    * changed daily tasks to tasks
    
    * updates to Navigation.test to test that navigation redirects to /tasks
    
    * update to direct to tasks not daily tasks in home.tsx
    
    * tas button directs to tasks
    
    * updated css names from daily-tasks to home
    
    * first draft error message page
    
    * tests for error-page
    
    * Change imaged to SVG and got rid of bottom empty content
    
    * removed svg from assets
    
    * Adjusted sizing of text
    
    * Sent invalid URL to redirect to Error page.
    
    * Made seperate component for ErrorSVG
    
    * adjust scss
    
    * Adjusted a test.
    
    * run test install and lint
    
    * git merge
    
    * Review ready.
    
    ---------
    
    Co-authored-by: Athish Thayalan <tathish@hotmail.co.uk>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import Home from "./pages/Home/Home";
 import "./styles/main.scss";
 import Calendar from "./pages/Calendar/Calendar";
 import { tribeUsers } from "./mockData/mockTribe";
 import { completedTasks } from "./mockData/mockCompletedTasks";
-import Leaderboard  from "./pages/Leaderboard/Leaderboard";
+import Leaderboard from "./pages/Leaderboard/Leaderboard";
 import Profile from "./pages/Profile/Profile";
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
       />
       <Route path="/leaderboard" element={<Leaderboard users={tribeUsers} />} />
       <Route path="/profile" element={<Profile user={tribeUsers[0]} />} />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 };
---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,24 @@
+import "./ErrorPage.scss";
+import Button from "../../components/Button/Button";
+import { Link } from "react-router-dom";
+import ErrorSVG from "../../components/ErrorSVG/ErrorSVG";
+
+const ErrorPage = () => {
+  return (
+    <div className="error-page">
+      <h1 className="error-page__heading">
+        OOPS.
+        <br />
+        LOOKS LIKE YOU'VE TAKEN A WRONG TURN.
+      </h1>
+      <ErrorSVG />
+      <Link to="/">
+        <div className="error-page__button">
+          <Button label={"Home"}></Button>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default ErrorPage;
