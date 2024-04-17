0e2f3f3fd3586f5d99f888b57290b2a5cd29ce8e
Michaela <michaelachan45@gmail.com>
2024-04-17 12:55:28 +0100

    fixed roboto error

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "@fontsource/poppins/300.css";
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/700.css";
-import "@fontsource/roboto/300.css";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
