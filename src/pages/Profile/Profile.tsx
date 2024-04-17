6262962f726c78c35a81d041f848ac4e21b858ef
Emma Oyetey <emmaflookis@hotmail.com>
2024-04-17 14:23:02 +0100

    adjusted localStorage removeItem to accessToken

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -23,7 +23,7 @@ const Profile = (
   
     try {signOut(auth);
         navigate("/auth");
-        localStorage.removeItem("userData");
+        localStorage.removeItem("accessToken");
         setUserUID (null);
   }
     catch (error) {
