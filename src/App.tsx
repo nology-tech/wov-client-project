bfd903b6cd706fb81c767b579a796090d2fe788a
Divya-Manivannan1 <reethudya@gmail.com>
2024-04-18 13:09:14 +0100

    pop-up done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import Home from "./pages/Home/Home";
 import "./styles/main.scss";
 import Calendar from "./pages/Calendar/Calendar";
-import { tribeUsers } from "./mockData/mockTribe";
 import ActiveTasks from "./pages/ActiveTasks/ActiveTasks";
 import Leaderboard from "./pages/Leaderboard/Leaderboard";
 import Profile from "./pages/Profile/Profile";
@@ -12,18 +11,45 @@ import Register from "./pages/Register/Register";
 import Account from "./pages/Account/Account";
 import { FirestoreProvider } from "./context/FirestoreProvider/FirestoreProvider";
 import UpdateProfile from "./pages/UpdateProfile/UpdateProfile";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { UserProfile } from "./types/User";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebase";
 
 const App = () => {
   const [userUID, setUserUID] = useState<null | string>(null);
   // NOTE: this console.log is used to workaround an eslint warning
   // It should be deleted once userUID is used
   console.log(userUID);
+  const [currentUser, setCurrentUser] = useState<UserProfile>({
+    id: "",
+    totalScore: 0,
+    name: "",
+    email: "",
+  });
+
+  const handleSetCurrentUser = (updatedUser:UserProfile) =>{
+    setCurrentUser({ ...currentUser, ...updatedUser})
+  }
 
   const handleSetUserUID = (userUID: string) => {
     setUserUID(userUID);
   };
 
+    const fetchCurrentUser = async () => {
+      try {
+        const userRef = doc(db, "test-tribe", "DrJZcEmb22Z5pG6fn2Fj2YYTHEy1");
+        const userDocSnap = await getDoc(userRef);
+        const user = userDocSnap.data() as UserProfile;
+        setCurrentUser(user);
+      } catch {
+        console.log("Error: Could not locate current user in the database");
+      }
+    };
+
+    useEffect(() => {
+      fetchCurrentUser();
+    }, []);
 
   return (
     <>
@@ -33,7 +59,10 @@ const App = () => {
           <Route path="/tasks" element={<ActiveTasks />} />
           <Route path="/calendar" element={<Calendar />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/profile" element={<Profile user={tribeUsers[0]} />} />
+          <Route
+            path="/profile"
+            element={<Profile user={currentUser} />}
+          />{" "}
           <Route
             path="/sign-in"
             element={<Login setUserUID={handleSetUserUID} />}
@@ -46,7 +75,7 @@ const App = () => {
           />
           <Route
             path="/edit"
-            element={<UpdateProfile currentUser={tribeUsers[0]} />}
+            element={<UpdateProfile currentUser={currentUser} setCurrentUser={handleSetCurrentUser} />}
           />
         </Routes>
       </FirestoreProvider>
--- src/pages/UpdateProfile/UpdateProfile.tsx
+++ src/pages/UpdateProfile/UpdateProfile.tsx
@@ -1,6 +1,6 @@
 import Button from "../../components/Button/Button";
 import "./UpdateProfile.scss";
-import { UserProfile } from "../../mockData/mockTribe";
+import { UserProfile } from "../../types/User";
 import Header from "../../components/Header/Header";
 import Navigation from "../../components/Navigation/Navigation";
 import TextField from "@mui/material/TextField";
@@ -8,14 +8,17 @@ import { ChangeEvent, useState } from "react";
 // import { Link } from "react-router-dom";
 import { doc, updateDoc } from "firebase/firestore";
 import { auth, db } from "../../firebase";
+import { signInWithEmailAndPassword, updatePassword } from "firebase/auth";
 import {
-  signInWithEmailAndPassword,
-  updatePassword,
-} from "firebase/auth";
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 
 type UpdateProfileProps = {
   currentUser: UserProfile;
-  setCurrentUser?: () => {};
+  setCurrentUser: (updatedUser: UserProfile) => void;
 };
 const UpdateProfile = ({ currentUser }: UpdateProfileProps) => {
   const [user, setUser] = useState<UserProfile>(currentUser);
@@ -23,7 +26,17 @@ const UpdateProfile = ({ currentUser }: UpdateProfileProps) => {
   const [currentPassword, setCurrentPassword] = useState<string>("");
   const [confirmPassword, setConfirmPassword] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string>("");
+  const [successMessage, setSuccessMessage] = useState<string>("");
+  const [open, setOpen] = useState<boolean>(false);
   const { img, name, bio, email } = user;
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const key = event.currentTarget.id;
@@ -69,35 +82,24 @@ const UpdateProfile = ({ currentUser }: UpdateProfileProps) => {
         currentPassword
       );
       await updatePassword(userPromise.user, password);
-
-      /*   .then(async () => {
-                  console.log("Sign In SuccessFul!");
-                  await updatePassword(auth.currentUser as User, password)
-                    .then(() => {
-                      console.log("password changed");
-                    })
-                    .catch(() => {
-                      setErrorMessage("Error in changing password");
-                    });
-                })
-                .catch(() => {
-                  setErrorMessage(
-                    "Error signing in. Please check the current password."
-                  );
-                }); */
       setErrorMessage("");
+      setSuccessMessage("Password successfully changed");
     } catch (error) {
       setErrorMessage((error as Error).message);
     }
+    handleClose();
+    setPassword("");
+    setConfirmPassword("");
+    setCurrentPassword("");
   };
 
   return (
     <div>
       <Header subtitle="Profile" />
-      <div className="profile">
-        <img src={img} className="profile__img" alt="Profile" />
-        <form className="profile__info">
-          <label htmlFor="name" className="profile__label">
+      <div className="profile-update">
+        <img src={img} className="profile-update__img" alt="Profile" />
+        <form className="profile-update__info">
+          <label htmlFor="name" className="profile-update__label">
             Name
           </label>
           <TextField
@@ -106,8 +108,9 @@ const UpdateProfile = ({ currentUser }: UpdateProfileProps) => {
             variant="outlined"
             onChange={handleChange}
             autoComplete="none"
+            className="profile-update__input"
           />
-          <label htmlFor="bio" className="profile__label">
+          <label htmlFor="bio" className="profile-update__label">
             Bio
           </label>
           <TextField
@@ -116,8 +119,9 @@ const UpdateProfile = ({ currentUser }: UpdateProfileProps) => {
             multiline
             onChange={handleChange}
             maxRows={4}
+            className="profile-update__input"
           />
-          <label htmlFor="email" className="profile__label">
+          <label htmlFor="email" className="profile-update__label">
             Email
           </label>
           <TextField
@@ -126,45 +130,76 @@ const UpdateProfile = ({ currentUser }: UpdateProfileProps) => {
             value={email}
             variant="outlined"
             disabled
-          />
-          <label htmlFor="current-password" className="profile__label">
-            Current Password
-          </label>
-          <TextField
-            id="current-password"
-            value={currentPassword}
-            variant="outlined"
-            onChange={handleCurrentPassword}
-            type="password"
-          />
-          <label htmlFor="password" className="profile__label">
-            Password
-          </label>
-          <TextField
-            id="password"
-            value={password}
-            variant="outlined"
-            onChange={handlePassword}
-            type="password"
-          />
-          <label htmlFor="confirmPassword" className="profile__label">
-            Confirm Password
-          </label>
-          <TextField
-            id="confirmPassword"
-            value={confirmPassword}
-            variant="outlined"
-            type="password"
-            onChange={handleConfirmPassword}
+            className="profile-update__input"
           />
         </form>
+
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle className="profile-update__popup-heading">
+            Change Password
+          </DialogTitle>
+          <DialogContent>
+            <form className="profile-update__popup-form">
+              <label
+                htmlFor="current-password"
+                className="profile-update__label"
+              >
+                Current Password
+              </label>
+              <TextField
+                id="current-password"
+                value={currentPassword}
+                variant="outlined"
+                onChange={handleCurrentPassword}
+                type="password"
+                className="profile-update__input"
+              />
+              <label htmlFor="password" className="profile-update__label">
+                New Password
+              </label>
+              <TextField
+                id="password"
+                value={password}
+                variant="outlined"
+                onChange={handlePassword}
+                type="password"
+                className="profile-update__input"
+              />
+              <label
+                htmlFor="confirmPassword"
+                className="profile-update__label"
+              >
+                Confirm Password
+              </label>
+              <TextField
+                id="confirmPassword"
+                value={confirmPassword}
+                variant="outlined"
+                type="password"
+                onChange={handleConfirmPassword}
+                className="profile-update__input"
+              />
+            </form>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              label={"UPDATE PASSWORD"}
+              variant={"light-grey"}
+              onClick={changePassword}
+            />
+          </DialogActions>
+        </Dialog>
+
         {errorMessage && (
-          <p className="register__error-message">{errorMessage}</p>
+          <p className="profile-update__error-message">{errorMessage}</p>
+        )}
+        {successMessage && (
+          <p className="profile-update__success-message">{successMessage}</p>
         )}
         <Button
           label={"CHANGE PASSWORD"}
           variant={"light-grey"}
-          onClick={changePassword}
+          onClick={handleClickOpen}
         />
         {/* <Link to="/profile"> */}
         <Button
