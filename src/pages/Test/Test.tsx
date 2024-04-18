e07088f947c9b1d91b544573495f4f14d0eb5576
Cheryl Lee <clee0937@gmail.com>
2024-04-18 14:25:22 +0100

    refactored code

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -16,46 +16,24 @@ function Test() {
 
   const storage = getStorage();
 
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const result = await getActiveTasks("OuZ1eeH9c5ZosgoXUi6Iraq7oM03");
-  //     setActiveTasks(result);
-  //   };
-  //   getData();
-  // }, [getActiveTasks]);
-
   useEffect(() => {
-    getDownloadURL(ref(storage, "videos/rabbit320.webm"))
-      .then((url) => {
-        // `url` is the download URL for 'videos/rabbit320.webm'
+    const fetchData = async () => {
+      try {
+        const url = await getDownloadURL(ref(storage, "videos/rabbit320.webm"));
 
-        //   // This can be downloaded directly:
-        //   const xhr = new XMLHttpRequest();
-        //   xhr.responseType = "blob";
-        //   xhr.onload = (event) => {
-        //     const blob = xhr.response;
-        //   };
-        //   xhr.open("GET", url);
-        //   xhr.send();
-
-        //Or inserted into an <img> element
         const video = document.querySelector<HTMLVideoElement>("video");
 
-        if (video === null) {
+        if (!video) {
           console.log("error with query selector");
-        } else {
-          video.setAttribute("src", url);
+          return;
         }
 
-        // save to videoSrc
-        let videoSrc = "";
-        videoSrc = url;
-        console.log("videoSrc", videoSrc);
-      })
-      .catch((error) => {
-        // Handle any errors
-        console.log(error);
-      });
+        video.setAttribute("src", url);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
   }, []);
 
   const uploadToDatabase = (url: string) => {
@@ -110,26 +88,6 @@ function Test() {
       <input type="file" name="video" onChange={handleOnChange} />
       <button onClick={() => handleClick()}>Upload Button</button>
       <video src="" width="800" height="400" controls />
-      {/* <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card> */}
     </div>
   );
 }
