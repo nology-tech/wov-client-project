19a426525d83a66b4b8daaea4fc524f701b30953
bwardak <157559558+bwardak@users.noreply.github.com>
2024-04-19 15:54:24 +0100

    Feature complete active task (#45)
    * Created Popup component and added base styles
    
    * Added logic to increase user points when tesk is completed
    
    * Added second popup on clicking 'ADD MEDIA' with a description box added.
    
    * Worked on completed tasks data. Commit for push.
    
    * Commit to pull from main pre-merged conflicts
    
    * User can remove a task from active tasks and see it on the calendar as a completed task
    
    * Refactor: create updateUserScore to update users score
    
    * Refactor: create removeActiveTask to make code modular
    
    * Refactor: create updateCompletedTask for modular code
    
    * Added tests for popup.tsx and new activetask.tsx features.
    
    * Add stories for the Popup component
    
    * Add dynamic date for completed tasks
    
    * For pull
    
    * Merged with main. Removed comments.
    
    * Removed comments. Utilised database import and premade types from their respective files. Fixed popup positioning bugs.
    
    * Updated custom render for tests that were not pushed yet.
    
    ---------
    
    Co-authored-by: Samuel Raducan <samuel.thembp@gmail.com>

--- src/pages/ActiveTasks/ActiveTasks.test.tsx
+++ src/pages/ActiveTasks/ActiveTasks.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import ActiveTasks from "./ActiveTasks";
 import { customRender } from "../../utils/testUtils";
@@ -68,5 +68,43 @@ describe("ActiveTask page component", () => {
         expect(screen.getByText(noTask)).not.toBeInTheDocument;
       });
     });
+  });
+
+  it('displays "Task Completed" popup', async () => {
+    customRender(<ActiveTasks />, {
+      useRouting: true,
+      firestoreValue: mockFireStore,
+    });
+
+    await waitFor(() => screen.getAllByTestId("active-task"));
+
+    const checkButtons = await screen.findAllByTestId("check-button");
+    userEvent.click(checkButtons[0]);
+
+    await waitFor(() =>
+      expect(screen.getByTestId("popup")).toHaveTextContent("Task Completed")
+    );
+  });
+
+  it('displays "Add Media" popup after opening first popup and clicking add media', async () => {
+    customRender(<ActiveTasks />, {
+      useRouting: true,
+      firestoreValue: mockFireStore,
+    });
+    await waitFor(() => screen.getAllByTestId("active-task"));
+
+    const checkButtons = await screen.findAllByTestId("check-button");
+    userEvent.click(checkButtons[0]);
+
+    await waitFor(() =>
+      expect(screen.getByTestId("popup")).toHaveTextContent("Task Completed")
+    );
+
+    const addMediaButton = screen.getByText("ADD MEDIA");
+    userEvent.click(addMediaButton);
+
+    await waitFor(() =>
+      expect(screen.getByTestId("popup")).toHaveTextContent("UPDATE TASK")
+    );
   });
 });
