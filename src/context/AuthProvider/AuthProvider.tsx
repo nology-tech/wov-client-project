25b4bf71956d549a460d23c7417fb8b0eb4afc7a
charlie-robin <charlie.robin.richardson@gmail.com>
2024-04-22 16:52:25 +0100

    (feat): Refactored Register & AuthProvider

--- src/context/AuthProvider/AuthProvider.tsx
+++ src/context/AuthProvider/AuthProvider.tsx
@@ -1,15 +1,47 @@
-import { AuthError, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  AuthError,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  UserCredential,
+} from "firebase/auth";
 import React, { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../firebase";
+import { NewUser, UserProfile } from "../../types/User";
+import {
+  createDocumentInFirestoreCollection,
+  FirestoreCollections,
+  getDocumentFromFirestoreCollection,
+  saveFileAndRetrieveDownloadUrl,
+  updateDocumentInFirestoreCollection,
+} from "../../utils/dbUtils";
+import { capitalisedFirstLetters } from "../../utils/capitalisedFirstLetters";
 
 type PromiseObjectNullString = Promise<{ error: null | string }>;
+
+type UserLoading = UserProfile & { loading: true };
+
+const userLoading: UserLoading = {
+  id: "",
+  totalScore: 0,
+  name: "",
+  email: "",
+  tribe: "",
+  loading: true,
+};
 
 export type AuthContextProps = {
   isAuthenticated: boolean;
   loginUser: (email: string, password: string) => PromiseObjectNullString;
   logoutUser: () => void;
-  userUID: string | null;
+  getUser: () => UserProfile | UserLoading;
+  createUser: (newUser: NewUser, profilePic?: File) => PromiseObjectNullString;
+  updateUser: (
+    data:
+      | Pick<UserProfile, "bio" | "name" | "email">
+      | Pick<UserProfile, "totalScore">
+  ) => PromiseObjectNullString;
 };
 
 export const AuthContext = createContext<AuthContextProps | undefined>(
@@ -21,12 +53,13 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
 }) => {
   const navigate = useNavigate();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userUID, setUserUID] = useState<string | null>(null);
+  const [user, setUser] = useState<UserProfile | null>(null);
 
   useEffect(() => {
-    if (localStorage.getItem("userUID")) {
+    const localStorageUID = localStorage.getItem("userUID");
+    if (localStorageUID) {
       setIsAuthenticated(true);
-      setUserUID(localStorage.getItem("userUID"));
+      getUserFromFirestore(localStorageUID);
     } else {
       setIsAuthenticated(false);
     }
@@ -42,12 +75,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
         email,
         password
       );
-      const accessToken = await userCredential.user.getIdToken();
-      const userID = userCredential.user.uid;
-      setUserUID(userID);
-      localStorage.setItem("accessToken", accessToken);
-      localStorage.setItem("userUID", userID);
-      setIsAuthenticated(true);
+      updateAuthState(userCredential);
       navigate("/");
     } catch (error) {
       const errorCode = (error as AuthError).code;
@@ -62,18 +90,124 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
     return { error: null };
   };
 
+  const updateAuthState = async (userCredential: UserCredential) => {
+    const userID = userCredential.user.uid;
+    localStorage.setItem("userUID", userID);
+    if (!user) {
+      getUserFromFirestore(userID);
+    }
+    setIsAuthenticated(true);
+  };
+
+  const getUserFromFirestore = async (userID: string) => {
+    const storedUser = await getDocumentFromFirestoreCollection<UserProfile>(
+      FirestoreCollections.TRIBE,
+      userID
+    );
+    if (storedUser) {
+      setUser(storedUser);
+    }
+  };
+
   const logoutUser = () => {
     signOut(auth);
-    localStorage.removeItem("accessToken");
     localStorage.removeItem("userUID");
     setIsAuthenticated(false);
-    setUserUID(null);
+    setUser(userLoading);
     navigate("/auth");
+  };
+
+  const updateUser = async (
+    data:
+      | Pick<UserProfile, "bio" | "name" | "email">
+      | Pick<UserProfile, "totalScore">
+  ): PromiseObjectNullString => {
+    if (user === null) {
+      return { error: "No user stored" };
+    }
+
+    const { error, updated } = await updateDocumentInFirestoreCollection(
+      "test-users",
+      user.id,
+      data
+    );
+
+    if (updated) {
+      setUser({ ...user, ...data });
+    }
+    return { error };
+  };
+
+  const createUser = async (
+    { email, password, firstName, lastName, bio, tribe }: NewUser,
+    profileFile?: File
+  ): PromiseObjectNullString => {
+    try {
+      const userCredential: UserCredential =
+        await createUserWithEmailAndPassword(auth, email, password);
+      updateAuthState(userCredential);
+
+      const uid = userCredential.user.uid;
+
+      const userProfile = {
+        id: uid,
+        totalScore: 0,
+        name: `${capitalisedFirstLetters(firstName)} ${capitalisedFirstLetters(
+          lastName
+        )}`,
+        bio,
+        email,
+        tribe,
+        img: "",
+      };
+
+      if (profileFile) {
+        const filePath = `${uid}(${firstName}-${lastName})/images/profile`;
+        const { fileDownloadUrl, error } = await saveFileAndRetrieveDownloadUrl(
+          filePath,
+          profileFile,
+          false
+        );
+        if (error) {
+          throw new Error(error);
+        }
+        userProfile.img = fileDownloadUrl || "";
+      }
+
+      await createDocumentInFirestoreCollection("test-users", uid, userProfile);
+      await createDocumentInFirestoreCollection("test-completed-tasks", uid, {
+        completedTasks: [],
+      });
+      await createDocumentInFirestoreCollection("test-active-tasks", uid, {
+        activeTasks: [],
+      });
+
+      setUser(userProfile);
+      navigate("/");
+      return { error: null };
+    } catch (error) {
+      return { error: (error as Error).message };
+    }
+  };
+
+  const getUser = () => {
+    if (user) {
+      return user;
+    } else {
+      return userLoading;
+    }
   };
 
   return (
     <AuthContext.Provider
-      value={{ isAuthenticated, loginUser, logoutUser, userUID }}
+      value={{
+        getUser,
+        isAuthenticated,
+        loginUser,
+        logoutUser,
+        createUser,
+        updateUser,
+      }}
     >
       {children}
     </AuthContext.Provider>
