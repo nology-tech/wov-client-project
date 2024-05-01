b73af7b2f44b64be4b1e96744eabc2f6cb271aa0
mattgibson18@outlook.com <mattgibson18@outlook.com>
2024-05-01 11:06:14 +0100

    adding routing to admin area

---
+++ src/pages/AdminDashboard/AdminDashboard.tsx
@@ -0,0 +1,10 @@
+const AdminDashboard = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <p>This is the admin dashboard. This area is under construction.</p>
+    </div>
+  );
+};
+
+export default AdminDashboard;
