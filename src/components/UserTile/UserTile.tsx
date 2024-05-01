7dfc70d9703cc239adb7e7590af5a38830405d8d
rosaTome <tomerosalinda@gmail.com>
2024-05-01 16:58:57 +0100

    tsx, scss and stories complete for UserTile

--- src/components/UserTile/UserTile.tsx
+++ src/components/UserTile/UserTile.tsx
@@ -12,10 +12,12 @@ const UserTile = ({ image, name, points, tribe, memberSince } : UserTileProps) =
 
   return (
     <div className="user-tile">
-      <img className="user-tile__image" src={image}/> 
-      <h4 className="user-tile__name">{"Name: " + name}</h4>
-      <p className="user-tile__points">{"Points: " + points}</p>
-      <p className="user-tile__tribe">{"Current Tribe: " + tribe}</p>
+      <img className="user-tile__image" src={image ?? "../../public/assets/images/default-profile-image.png"}/> 
+      <div className="user-tile__info">
+        <h4 className="user-tile__name">{"Name: " + name}</h4>
+        <p className="user-tile__points">{"Points: " + points}</p>
+        <p className="user-tile__tribe">{"Current Tribe: " + tribe}</p>
+      </div>
       <p className="user-tile__memberSince">{"Member since: " + memberSince}</p>
     </div>
   )
