fc7a51aa1660bccbf9bbd20b86932602c2633787
Molly Saleh <Molly@192.168.1.206>
2024-05-01 16:15:37 +0100

    added second test for profile image

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -23,4 +23,19 @@ describe.only("should pass Header component tests", () => {
     expect(profileImg).toBeInTheDocument();
     expect(profileImg).toHaveProperty("src", "../../assets/images/arrow-left.png")
   });
+
+  it("should user profile image if one exists", () => {
+    customRender(
+      <Header
+        subtitle={""}
+
+      />
+    );
+
+    const profileImg = screen.getByLabelText("profile picture link");
+    console.log(profileImg)
+
+    expect(profileImg).toBeInTheDocument();
+    expect(profileImg).toHaveProperty("src", "../../assets/images/default-profile-image.png")
+  });
 });
