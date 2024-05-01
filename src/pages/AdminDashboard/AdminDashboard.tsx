e1aa44ccb4a9535ecf34b58a7945f88db84d10ed
mattgibson18@outlook.com <mattgibson18@outlook.com>
2024-05-01 11:25:34 +0100

    adding logout capability

--- src/pages/AdminDashboard/AdminDashboard.tsx
+++ src/pages/AdminDashboard/AdminDashboard.tsx
@@ -1,8 +1,16 @@
+import Button from "../../components/Button/Button";
+import { useAuth } from "../../hooks/useAuth";
+
 const AdminDashboard = () => {
+  const { logoutUser } = useAuth();
+
   return (
     <div>
       <h1>Admin Dashboard</h1>
-      <p>This is the admin dashboard. This area is under construction.</p>
+      <p>This is the admin dashboard</p>
+      <div>
+        <Button label={"SIGN OUT"} variant={"secondary"} onClick={logoutUser} />
+      </div>
     </div>
   );
 };
