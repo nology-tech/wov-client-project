b29b689162c4b1ab55e6e250e85f5777fd07a9a6
Byasar3 <bernayasar@hotmail.co.uk>
2024-05-02 15:32:36 +0100

    added missing dependency

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,27 +1,21 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from "react";
 
 type LoadingProps = {
-    loading: boolean;
-    setLoading: React.Dispatch<React.SetStateAction<boolean>>;
-    isAuthenticated: boolean
-}
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  isAuthenticated: boolean;
+};
 
-const Loading = ({loading, setLoading, isAuthenticated}: LoadingProps) => {
+const Loading = ({ loading, setLoading, isAuthenticated }: LoadingProps) => {
+  useEffect(() => {
+    setLoading(false);
+  }, [isAuthenticated, setLoading]);
 
-    useEffect(() => {
-        
-        setLoading(false);
-        }, [isAuthenticated]);
-
-    if (loading) {
+  if (loading) {
     return null;
-    }
+  }
 
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return <div></div>;
+};
 
-export default Loading
+export default Loading;
