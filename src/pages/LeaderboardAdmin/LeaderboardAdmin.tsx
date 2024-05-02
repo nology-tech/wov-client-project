9d3941c298ba44132a07f2909a528f72736fbecd
Byasar3 <bernayasar@hotmail.co.uk>
2024-05-02 10:14:01 +0100

    created dropmenu

--- src/pages/LeaderboardAdmin/LeaderboardAdmin.tsx
+++ src/pages/LeaderboardAdmin/LeaderboardAdmin.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+import DropdownMenu from "../../components/DropdownMenu/DropdownMenu";
 import Header from "../../components/Header/Header";
 import NavigationAdmin from "../../components/NavigationAdmin/NavigationAdmin";
+import { SelectChangeEvent } from "@mui/material";
 
 const LeaderboardAdmin = () => {
+  const [tribe, setTribe] = useState<string>("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setTribe(event.target.value as string);
+  };
+
   return (
     <div className="leaderboard-admin">
       {/* header */}
-      <Header subtitle="Leaderboard" profileImage="user's img"/>
+      <Header subtitle="Leaderboard" profileImage="user's img" />
 
       {/* drop down of tribes */}
+      <DropdownMenu tribe={tribe} handleChange={handleChange} />
 
       {/* list of people and scores from selected tribe */}
 
@@ -17,6 +27,6 @@ const LeaderboardAdmin = () => {
       </div>
     </div>
   );
-}
+};
 
-export default LeaderboardAdmin
+export default LeaderboardAdmin;
