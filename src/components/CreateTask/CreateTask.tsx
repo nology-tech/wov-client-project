2b484793517756c99c791477944091626a96bdc9
Bethany Burrows <jedcburrows@gmail.com>
2024-05-02 12:25:55 +0100

    Created initial functionality for creating task in app and pushing to firebase.

--- src/components/CreateTask/CreateTask.tsx
+++ src/components/CreateTask/CreateTask.tsx
@@ -1,6 +1,8 @@
 import { ChangeEvent, useState } from "react";
 import Button from "../Button/Button";
 import "./CreateTask.scss";
+import { createDocumentInFirestoreCollection } from "../../utils/dbUtils";
+import { FirestoreCollections } from "../../utils/dbUtils";
 
 type CreateTaskProps = {
     buttonLabel: string;
@@ -17,10 +19,13 @@ points: "",
 export const CreateTask = ({buttonLabel}:CreateTaskProps) => {
 const [formData, setFormData] = useState(emptyFormData)
 const [missingFieldsError, setMissingFieldsError] = useState<string>("");
+const localStorageUID = localStorage.getItem("userUID");
 
-const handleCreateTask = () => {
-    if (formData.name && formData.date && formData.category && formData.description && formData.points) {
+const handleCreateTask = async () => {
+    if (formData.name && formData.date && formData.category && formData.description && formData.points && localStorageUID) {
         setMissingFieldsError("")
+        await createDocumentInFirestoreCollection(FirestoreCollections.TASKS, localStorageUID, formData)
+        await setFormData(emptyFormData)
     } else {
         setMissingFieldsError("Please fill all required fields.");
     }
@@ -36,11 +41,11 @@ const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         <section className="create-task">
             <div className="create-task__form">
                 <form action="">
-                    <label>Name</label><input name="name" type="text" onChange={handleChange} data-testid = "name-input"/>
-                    <label>Date</label><input name="date" type="Date" onChange={handleChange} min={new Date().toISOString().split('T')[0]}/>
-                    <label>Category</label><input name="category" type="text" onChange={handleChange}/>
-                    <label>Description</label><input name="description" type="text" onChange={handleChange}/>
-                    <label>Points</label><input name="points" type="text" onChange={handleChange}/>
+                    <label>Name</label><input name="name" type="text" onChange={handleChange} data-testid = "name-input" value={formData.name}/>
+                    <label>Date</label><input name="date" type="Date" onChange={handleChange} min={new Date().toISOString().split('T')[0]} value={formData.date}/>
+                    <label>Category</label><input name="category" type="text" onChange={handleChange} value={formData.category}/>
+                    <label>Description</label><input name="description" type="text" onChange={handleChange} value={formData.description}/>
+                    <label>Points</label><input name="points" type="text" onChange={handleChange} value={formData.points}/>
                     {missingFieldsError && (
                     <p>
                         {missingFieldsError}
