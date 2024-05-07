2d816da77c503223268fa3319fea777a2c030b4b
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2024-05-07 14:22:12 +0100

    made the conditional rendering work for task and group buttons

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,24 +1,25 @@
-import Header from "../../components/Header/Header"
-import NavigationAdmin from "../../components/NavigationAdmin/NavigationAdmin"
-import ButtonContainer from "../../containers/ButtonContainer/ButtonContainer"
-import Groups from "../../containers/Groups/Groups"
-import Tasks from "../../containers/Tasks/Tasks"
-import groups from "../../mockData/groups"
-import "./DashBoard.scss"
-
-
+import { useState } from "react";
+import Header from "../../components/Header/Header";
+import NavigationAdmin from "../../components/NavigationAdmin/NavigationAdmin";
+import ButtonContainer from "../../containers/ButtonContainer/ButtonContainer";
+import Groups from "../../containers/Groups/Groups";
+import Tasks from "../../containers/Tasks/Tasks";
+import groups from "../../mockData/groups";
+import "./DashBoard.scss";
 
 const Dashboard = () => {
+  const [showGroup, setShowGroup] = useState(true);
+  const [showTask, setShowTask] = useState(false);
+
   return (
     <div className="dashboard">
-        <Header subtitle={"Dashboard"}/>
-        <ButtonContainer/>
-        <Groups groups={groups}/>
-        <Tasks/>
-        <NavigationAdmin navActionIndex={0}/>
-      
+      <Header subtitle={"Dashboard"} />
+      <ButtonContainer setShowGroup={setShowGroup} setShowTask={setShowTask} />
+      {showGroup && <Groups groups={groups} />}
+      {showTask && <Tasks />}
+      <NavigationAdmin navActionIndex={0} />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
