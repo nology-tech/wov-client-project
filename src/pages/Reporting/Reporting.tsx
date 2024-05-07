da29b774ac06d57340ff79894a03070e634a0ed2
Byasar3 <bernayasar@hotmail.co.uk>
2024-05-07 17:09:20 +0100

    removed unused react import

--- src/pages/Reporting/Reporting.tsx
+++ src/pages/Reporting/Reporting.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "../../components/Header/Header";
 import NavigationAdmin from "../../components/NavigationAdmin/NavigationAdmin";
 import ReportingTile from "../../components/ReportingTile/ReportingTile";
