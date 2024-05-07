5c32d386f4efa5106e40bfe72b16f2e7fc275e29
Byasar3 <bernayasar@hotmail.co.uk>
2024-05-07 16:02:44 +0100

    reporting page set up

---
+++ src/pages/Reporting/Reporting.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import NavigationAdmin from "../../components/NavigationAdmin/NavigationAdmin";
+
+const Reporting = () => {
+  return (
+    <div>
+      <Header subtitle="Reporting" />
+      Reporting
+	  {/* tiles that will contain what the information will be and the box will link to  */}
+	  {/* tiles: most active user, users that haven't signed in, users with streaks of certain activities (eg completing task x amount of times) */}
+	  {/* tile component and will be reused and will map over the data from the db*/}
+      <NavigationAdmin navActionIndex={2} />
+    </div>
+  );
+};
+
+export default Reporting;
