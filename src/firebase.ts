e7d43ec65db5497b17eb0b277a9d83d0cf930242
Lidia Haile <lidiahaile@gmail.com>
2024-05-09 09:29:38 +0100

    data base changed

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,12 +10,12 @@ import { getStorage } from "firebase/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_TOKEN,
-  authDomain: "wov2-5c8de.firebaseapp.com",
-  projectId: "wov2-5c8de",
-  storageBucket: "wov2-5c8de.appspot.com",
-  messagingSenderId: "916096577110",
-  appId: "1:916096577110:web:8be429ecc4f99bc38515f7",
-  measurementId: "G-EHN5Z1ZN1P",
+  authDomain: "wov-client-project-66242.firebaseapp.com",
+  projectId: "wov-client-project-66242",
+  storageBucket: "wov-client-project-66242.appspot.com",
+  messagingSenderId: "1084549732009",
+  appId: "1:1084549732009:web:d75fd1c6e622b7b1187bb8",
+  measurementId: "G-L1F9662PYK",
 };
 
 // Initialize Firebase
