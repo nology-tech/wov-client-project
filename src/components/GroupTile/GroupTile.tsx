d8dae891df4c0fd59e44e65443bd8dd88b250d3e
Faridah Salami <f.salami@outlook.com>
2024-05-10 11:25:43 +0100

    static delete button implemented

--- src/components/GroupTile/GroupTile.tsx
+++ src/components/GroupTile/GroupTile.tsx
@@ -1,3 +1,4 @@
+import Button from "../Button/Button";
 import "./GroupTile.scss";
 
 type GroupTileProps = {
@@ -15,14 +16,23 @@ const GroupTile = ({
 }: GroupTileProps) => {
   const date = dateGroupStarted;
 
+  const handleDeleteGroup = () => {
+    console.log("delete has been clicked");
+    
+  }
+
   return (
+    <>
       <div className="group-tile">
         <h3 className="group-tile__name">{tribeName}</h3>
         <p className="group-tile__members">Members: {numberOfMembers}</p>
         <p className="group-tile__points">Total Points: {totalPoints}</p>
         <p className="group-tile__date">Date created: {date}</p>
         <button className="group-tile__editButton">EDIT</button>
+        <Button label={"Delete"} variant="light-grey" size="small" onClick={handleDeleteGroup}/>
       </div>
+    </>
+
   );
 };
 
