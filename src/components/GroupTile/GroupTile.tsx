8211c977defda3a632678ec2275ee2d4149470cd
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2024-05-10 15:53:43 +0100

    styled delete button

--- src/components/GroupTile/GroupTile.tsx
+++ src/components/GroupTile/GroupTile.tsx
@@ -1,8 +1,7 @@
-import { MouseEventHandler, useEffect, useState } from "react";
+import { useState } from "react";
 import { useFirestore } from "../../hooks/useFireStore";
 import Button from "../Button/Button";
 import "./GroupTile.scss";
-import { count } from "firebase/firestore";
 
 type GroupTileProps = {
   tribeName: string;
@@ -25,7 +24,7 @@ const GroupTile = ({
   const [hasPressedDelete, setHasPressedDelete] = useState(false);
   const [countdown, setCountdown] = useState(3);
 
-  const handleDelete = (): MouseEventHandler<HTMLButtonElement> => {
+  const handleDelete = () => {
     setHasPressedDelete(true);
 
     if (countdown == 1) {
@@ -39,18 +38,19 @@ const GroupTile = ({
   return (
     <>
       <div className="group-tile">
-        <h3 className="group-tile__name">{tribeName}</h3>
-        <p className="group-tile__members">Members: {numberOfMembers}</p>
-        <p className="group-tile__points">Total Points: {totalPoints}</p>
-        <p className="group-tile__date">Date created: {date}</p>
-        <button className="group-tile__editButton">EDIT</button>
-        {hasPressedDelete ? <p>Will delete in {countdown} presses</p> : <></>}
-        <Button
-          label={"Delete"}
-          variant="light-grey"
-          size="small"
-          onClick={handleDelete}
-        />
+        <div className="group-tile__contents">
+          <h3 className="group-tile__name">{tribeName}</h3>
+          <p className="group-tile__members">Members: {numberOfMembers}</p>
+          <p className="group-tile__points">Total Points: {totalPoints}</p>
+          <p className="group-tile__date">Date created: {date}</p>
+        </div>
+        <div className="group-tile__button-container">
+          <button className="group-tile__editButton">EDIT</button>
+          <button className="group-tile__editButton" onClick={handleDelete}>
+            DELETE
+          </button>
+          {hasPressedDelete ? <p>Will delete in {countdown} presses</p> : <></>}
+        </div>
       </div>
     </>
   );
