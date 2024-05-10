b0b663b920aee408b9c5df50295ebf786984c1d6
mattgibson18@outlook.com <mattgibson18@outlook.com>
2024-05-10 11:35:16 +0100

    added placeholder for user streaks page

--- src/pages/ReportingUserStreaks/ReportingUserStreaks.tsx
+++ src/pages/ReportingUserStreaks/ReportingUserStreaks.tsx
@@ -1,43 +1,12 @@
-import { UserProfile } from "../../types/User";
-import { useState, useCallback, useEffect } from "react";
 import Header from "../../components/Header/Header";
 import NavigationAdmin from "../../components/NavigationAdmin/NavigationAdmin";
-import { getCollectionFromFirestore, FirestoreCollections } from "../../utils/dbUtils";
+import "./ReportingUserStreaks.scss";
 
 const ReportingUserStreaks = () => {
-  // new database:
-  // users now have an array of 'active tasks' and array of 'completed tasks' 
-
-  const [users, setUsers] = useState<UserProfile[]>([]);
-
-  const fetchAllUsers = useCallback(async () => {
-    const fetchedUsers = await getCollectionFromFirestore<UserProfile>(
-      FirestoreCollections.USERS
-    );
-
-    if (fetchedUsers) {
-      setUsers(fetchedUsers);
-    } else {
-      console.error("No users found in 'test-tribe' collection.");
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchAllUsers();
-  }, [fetchAllUsers]);
-
-  const sortUsersByStreakCount = (users: UserProfile[]) => {
-
-    return 
-  }
-
-
   return (
-    <div>
+    <div className="usersteaks">
       <Header subtitle="User Streaks" />
-      <div>
-        {/* card with user's: id, name, img, task name, task streak count */}
-      </div>
+      <div className="userstreaks__info">Coming Soon!</div>
       <NavigationAdmin navActionIndex={2} />
     </div>
   );
