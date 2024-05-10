723d2d442dc68180bdad7e2846a20af7e5edddd8
mattgibson18@outlook.com <mattgibson18@outlook.com>
2024-05-10 12:36:49 +0100

    fixed default image on admin leaderboard

--- src/pages/LeaderboardAdmin/LeaderboardAdmin.tsx
+++ src/pages/LeaderboardAdmin/LeaderboardAdmin.tsx
@@ -1,4 +1,4 @@
-import { useState,  useEffect} from "react";
+import { useState, useEffect } from "react";
 import DropdownMenu from "../../components/DropdownMenu/DropdownMenu";
 import Header from "../../components/Header/Header";
 import NavigationAdmin from "../../components/NavigationAdmin/NavigationAdmin";
@@ -9,18 +9,17 @@ import { UserProfile } from "../../types/User";
 import LeaderboardCard from "../../components/LeaderboardCard/LeaderboardCard";
 import { GroupData } from "../../types/Groups";
 
-
 const LeaderboardAdmin = () => {
-  const {getUser} = useAuth()
+  const { getUser } = useAuth();
   const user = getUser();
-  const {getTribes} = useFirestore()
+  const { getTribes } = useFirestore();
   const { getLeaderboard } = useFirestore();
   const [tribe, setTribe] = useState<GroupData[]>([]);
   const [users, setUsers] = useState<UserProfile[]>([]);
-  const [chosenTribe, setChosenTribe] = useState<string>("")
+  const [chosenTribe, setChosenTribe] = useState<string>("");
 
   const handleChange = (event: SelectChangeEvent) => {
-    setChosenTribe(event.target.value)
+    setChosenTribe(event.target.value);
   };
 
   useEffect(() => {
@@ -35,11 +34,9 @@ const LeaderboardAdmin = () => {
     const getLeaderboardData = async () => {
       const result = await getLeaderboard(chosenTribe);
       setUsers(result);
-
     };
-    getLeaderboardData();  
+    getLeaderboardData();
   }, [getLeaderboard, user, chosenTribe]);
-
 
   const sortUserByScore = () => {
     const sortedUsers = [...users.map((user) => ({ ...user }))].sort((a, b) => {
@@ -51,7 +48,6 @@ const LeaderboardAdmin = () => {
     });
     return sortedUsers;
   };
-  
 
   return (
     <div className="leaderboard-admin">
@@ -59,24 +55,29 @@ const LeaderboardAdmin = () => {
       <Header subtitle="Leaderboard" profileImage="user's img" />
 
       {/* drop down of tribes */}
-      
-      <DropdownMenu arrayOfGroups={tribe} chosenTribe={chosenTribe} handleChange={handleChange} />
+
+      <DropdownMenu
+        arrayOfGroups={tribe}
+        chosenTribe={chosenTribe}
+        handleChange={handleChange}
+      />
 
       {/* list of people and scores from selected tribe */}
       {sortUserByScore().map((sortedUser, index) => (
-
         <LeaderboardCard
           key={sortedUser.id + sortedUser.name}
           name={sortedUser.name}
           profileImage={
-            sortedUser.img ?? "./assets/images/default-profile-image.png"
+            sortedUser.img
+              ? sortedUser.img
+              : "./assets/images/default-profile-image.png"
           }
           totalScore={sortedUser.totalScore}
           isFirstCard={index === 0}
           userID={sortedUser.id}
           currentUserID={user.id}
         />
-        ))}
+      ))}
       <div>
         <NavigationAdmin navActionIndex={4} />
       </div>
